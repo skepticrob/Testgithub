7f4254e729ae77e9be9dd2f8c895d1aaf3aed651
skepticrob <roberto.semblante@gmail.com>
2014-09-18 12:12:58 +0800

    Init project,simple index

---
+++ rob/index.php
@@ -0,0 +1,409 @@
+
+<?php
+
+/* Programmer:Roberto Semblante Jr.
+   Date-Started:Jan 20,2014
+   Project:School Management System    
+*/
+
+
+include_once('header.php');
+
+
+
+?>
+
+
+<body  onload="$('#title_block').fadeIn(5000);" style="magrin:0px; padding:0px;">
+
+
+</div>  
+
+
+<style>
+iframe{
+margin-left:10px;
+}
+
+
+.links{
+
+color:#447F2B1; 
+
+font-weight:bold;
+
+text-decoration:none;
+
+border-radius:20px;
+
+padding: 10px;
+
+color: #FFF;
+
+text-decoration: none;
+
+background: #1e5799; 
+
+background: -moz-linear-gradient(top, #1e5799 0%, #2989d8 50%, #207cca 51%, #7db9e8 100%); 
+
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#1e5799), color-stop(50%,#2989d8), color-stop(51%,#207cca), color-stop(100%,#7db9e8));
+
+background: -webkit-linear-gradient(top, #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); 
+
+background: -o-linear-gradient(top, #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%);
+
+background: -ms-linear-gradient(top, #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%);
+
+background: linear-gradient(to bottom, #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); 
+
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#7db9e8',GradientType=0 ); 
+
+
+}
+#news_bulletin_title{
+border-radius:20px;
+background: #ffff88; /* Old browsers */
+background: -moz-linear-gradient(top, #ffff88 0%, #ffff88 100%); /* FF3.6+ */
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#ffff88), color-stop(100%,#ffff88)); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(top, #ffff88 0%,#ffff88 100%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(top, #ffff88 0%,#ffff88 100%); /* Opera 11.10+ */
+background: -ms-linear-gradient(top, #ffff88 0%,#ffff88 100%); /* IE10+ */
+background: linear-gradient(to bottom, #ffff88 0%,#ffff88 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffff88', endColorstr='#ffff88',GradientType=0 ); /* IE6-9 *//
+
+border-style:solid;
+border-width:1px; 
+}
+#School_events{
+
+border-width:1px; 
+border-radius:20px;
+background: #ffff88; 
+background: -moz-linear-gradient(top, #ffff88 0%, #ffff88 100%); 
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#ffff88), color-stop(100%,#ffff88)); 
+background: -webkit-linear-gradient(top, #ffff88 0%,#ffff88 100%); 
+background: -o-linear-gradient(top, #ffff88 0%,#ffff88 100%); 
+background: -ms-linear-gradient(top, #ffff88 0%,#ffff88 100%); 
+background: linear-gradient(to bottom, #ffff88 0%,#ffff88 100%); 
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffff88', endColorstr='#ffff88',GradientType=0 ); 
+}
+#Maranatha_Advertisement{
+border-radius:20px;
+background-color:#A8DCF4;
+
+}
+
+</style>
+
+<div class="resizer">
+
+
+<div class="container_12">
+   </br>
+			
+			</br>
+			</br>
+<section id="nav_section">
+<nav  id='nav_menu'>
+<ul>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   <li><a href='index.php'>Home</a></li>
+   <li><a href='#'><span>Gallery</span></a></li>
+   <li class='last'><a href='http://www.tokopals.com/kjvExplorationSite/Beta/'><span>Bible Reading</span></a></li>
+   <li><a href='#'><span>Mission/Vision</span></a></li>
+   <li class='last'><a href='Aboutus.php' title="About Us"><span>About Us</span></a></li>
+   <li class='last'><a href='contactus.php'><span>Contact Us</span></a></li>
+  
+   
+   
+</ul>
+</section>
+	
+</nav>
+	<div class="clear"></div>
+  <div class="grid_12">
+  <style>
+#banner
+{
+
+}
+</style>
+  <?php
+  
+  
+  
+                           $result_random = rand(1, 100); 
+  
+  if($result_random <= 70){
+  
+                           print('<img id="banner" src="img/Banner.jpg" />');
+}
+
+
+                            else if($result_random <= 90){
+
+  print('<img id="banner" src="img/Banner.jpg" />');
+
+
+}
+
+                                                      else {
+
+ print('<img id="banner" src="img/Banner.jpg" />');
+
+
+                                                           } 
+  
+  
+  
+  ?>
+  
+  
+		
+  </div>
+</div>
+</div>
+  
+<div class="container_12"> 
+
+ 
+  
+  
+  <div class="clear"></div>
+  
+  <section id="title_block" onclick="$(this).effect('shake');" style="margin:0px; padding:0px;">
+
+	</section>
+	
+  <div class="grid_8">
+  <br/>
+
+	<section id="rightcol" style="border-style:none;">
+	
+
+	
+	
+	
+		
+<iframe src="iframe.php"  height="305em" width="604px" scrolling="no" frameborder="0" style="margin:auto border-style:solid;
+border-width:1px;" ></iframe>
+		
+<br/>
+<br/>
+
+		<p class="text"  style="background: none repeat scroll 0% 0% #FFF; margin: 0px auto; text-align: left;">
+		
+			The(4th Watch)Maranatha Christian School of Lapu-Lapu  City Inc is non-stock
+		sectarian and non-profit institution which offers quality but low cost education
+		that seeks to form persons with high regard for Christian Values.
+		
+
+		  Established and dedicated  to provide quality  and relevant education which is
+		  accessible to all emphasis on Christian Living. This School is more than a 
+		  Educational institution that is committed to love and motivate every pupil to 
+		  their greatest potential in achieving character excellence.
+		
+		   It is a ministry established initially in 1980 as non-formal education. The 
+		 institution was pioneered by Leticia S.Ferriol,Ed.D. with the aim of providing 
+		 quality education that will meet the community and global competitiveness geared
+		 towards the development of intellectual,social,moral and above all,spiritual being.
+		  
+		
+		
+		</p>
+	
+	
+	</section>
+  
+  </div>
+  
+  
+  
+  
+  <div class="grid_4" >
+  
+	<section id="leftcolumn" style="border-style:none; " >
+		<br/>
+		<input type="text" name="Searchmain" id="searchmain" placeholder="Search Here!"/>
+		<br/>
+		<br/>
+		<h2  id="news_bulletin_title" ><center>News Bulletin </center></h2>
+
+		<div style="width:299px; height:259px;" id="news_bulletin" style="overflow:scrolling">
+				<p>
+		
+			<marquee direction="down" height="233px"; width="299px"; scrolldelay="500"; >
+			
+			<?php
+			
+			/*Bulletin Post Sched*/
+			
+			
+			include_once('databaseconfig.php');
+			
+			$post = mysql_query("SELECT * FROM `newsbulletin`");
+			
+			for($counter=0;$sql= mysql_fetch_array($post);$counter ++){
+				    extract($sql);
+			
+			         
+			}
+        
+			
+
+			$result = mysql_query("SELECT * FROM `newsbulletin` WHERE id ='$id'") or die(mysql_error());
+
+            while($row = mysql_fetch_array($result)) :
+            extract($row);
+ 
+                          
+						  echo"<center>".$bulletinpost."</center>";
+						  
+                          echo"<center><p>Post By:".$person."</p>";
+
+                                             endwhile;
+
+ ?>			
+			
+			
+ </marquee>
+		</p>
+		
+		</div>
+		<br/>
+
+	<?php
+
+/*XML FOR SCHOOL SCHEDULES*/
+
+$mix_schedules = "<?xml version='1.0'?>
+											   <MaranathaSched>
+											   <title>School Schedule</title>
+											   <schedulinglist>
+												<scheduling>
+												<time>7:00-8:00
+												</time>
+												<subject>Math
+												</subject>
+												
+											</scheduling>
+
+											<scheduling>
+												<time>8:00-9:00
+												</time>
+												
+												<subject>English
+												</subject>
+												
+											</scheduling>
+											
+
+											<scheduling>
+												<time>9:00-10:00
+												</time>
+												
+												<subject>Computer:
+												</subject>
+												
+											</scheduling>
+
+											<scheduling>
+												<time>10:00-11:00
+												</time>
+												
+												<subject>C.L.E:
+												</subject>
+												
+											</scheduling>
+											
+													
+										</schedulinglist>
+										
+	    <schedList>	
+		<afternoon> Not Available
+		</afternoon>
+
+		<afternoon>  Not Available
+		</afternoon>		
+
+
+
+		<afternoon> Not Available
+		</afternoon>		
+			
+	</schedList>		
+	
+</MaranathaSched>
+
+";
+/*end of xml school schedule*/
+
+$subject = simplexml_load_string($mix_schedules) or die('Schedule does not exsist');
+
+
+
+                          echo "<div id=\"school_sked\" style=\"color:#fffbf0; width: 299px; border: 1px dotted #DBEAF9;\" >";
+						  
+
+
+/*calling the title in xml */
+
+                         echo "<br><h3><center>".$subject->{title}."</h3></center><p><center>Morning Schedule:</center></p><ul>";
+						 
+
+foreach ( $subject->{schedulinglist}->{scheduling} as $scheduling ):
+
+	echo "<li>".$scheduling->{time}." : ".$scheduling->{subject}."</li>";
+	
+endforeach;
+
+
+
+echo "</ul>";
+
+echo "<p><center>Afternoon Schedule:</center></p><ul>";
+
+foreach ( $subject->{schedList}->{afternoon} as $direction ):
+
+					echo "<li>".$direction."</li>";
+	
+endforeach;
+
+
+
+echo "</ol></div>";
+
+?>
+
+</p>
+	
+	<h2 id="School_events"> School Events </h2>
+<video width="300" height="240" controls autoplay>
+  <source src="movie.ogg" type="video/ogg">
+  <source src="movie.mp4" type="video/mp4">
+  <object data="movie.mp4" width="320" height="240">
+    <embed width="320" height="240" src="movie.swf">
+  </object>
+</video>
+		</p>
+		
+	<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=43" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:43px;" allowTransparency="true"></iframe>	
+		
+		
+		
+	</section>
+	
+		<div class="clear"></div>
+   </div>
+   <br/>
+    <br/>
+	<br/>
+   
+   <div class="grid_12">
+
+
+<?php
+
+require_once('footer.php');
+
+?>
